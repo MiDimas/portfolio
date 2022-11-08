6e5ccf6d21f355e7cd6be1a571930ed8e44214dc
MiDimas <103483149+MiDimas@users.noreply.github.com>
2022-11-08 12:48:39 +0800

    add script

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    function correctingSize(e){
+        let menu = document.getElementsByClassName("head-link-list")[0]
+        let bar = document.getElementsByClassName("bar")[0]
+        let hiddenMenu = document.getElementsByClassName("hidden-menu")[0]
+        if(isFinite(e)){
+            var sizeWindow = e
+        }
+        else{
+            var sizeWindow = e.target.innerWidth
+        }
+        if (sizeWindow > 600){
+            menu.classList.remove("none")
+            bar.classList.add("none")
+            hiddenMenu.classList.add("none")
+        }
+        else {
+            menu.classList.add("none")
+            bar.classList.remove("none")
+            hiddenMenu.classList.remove("none")
+        }
+    }
+    correctingSize(event.path[1].innerWidth)
+    window.addEventListener("resize", correctingSize)
+    let bar = document.getElementsByClassName("bar")[0]
+    bar.addEventListener("click", function(){
+        let hiddenMenu = document.getElementsByClassName("hidden-menu")[0]
+        hiddenMenu.classList.toggle("unvis-hidden-menu")
+        hiddenMenu.classList.toggle("vis-hidden-menu")
+
+    })
+})
